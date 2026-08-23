1140ffb82795d96b5f45a8135066b6b3f20c1fb1
Vuong <3168632+vuon9@users.noreply.github.com>
2026-08-23 14:37:41 +0700

    style: format MonacoHighlightedCode

--- frontend/src/components/inputs/MonacoHighlightedCode.jsx
+++ frontend/src/components/inputs/MonacoHighlightedCode.jsx
@@ -39,7 +39,6 @@ export default function MonacoHighlightedCode({
   useMonacoDevtoolboxTheme();
   const [ready, setReady] = useState(false);
 
-
   useEffect(() => {
     codeRef.current = code;
   });
